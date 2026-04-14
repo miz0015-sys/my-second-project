6ceb71f37afe2cd57e3ff227a699ac34bac7e25e
miz0015-sys <miz0015@mckinnonsc.vic.edu.au>
2026-04-14 19:00:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function isCollidingWithObstacle(px, py) {
     let dy = py - closestY;
 
     // Check if inside radius
-    if ((dx * dx + dy * dy) <= (player.radius * player.radius)) {
+(dx * dx + dy * dy) <= ((player.radius - 1) * (player.radius - 1))
       return true;
     }
   }
